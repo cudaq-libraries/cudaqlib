d9aa21b8e1730138c2010a03d4bec481a24a04ed
Alex McCaskey <amccaskey@nvidia.com>
2024-07-29 15:47:56 -0400

    Minor fixes and updates for chemistry workflows (#9)
    Signed-off-by: Alex McCaskey <amccaskey@nvidia.com>

--- python/tests/test_operators.py
+++ python/tests/test_operators.py
@@ -8,15 +8,22 @@
 
 import os
 
-import pytest
+import pytest, pathlib
 import numpy as np
 
 import cudaq, cudaqlib
 
+currentPath = pathlib.Path(__file__).parent.resolve()
+
+
 def test_operators():
     geometry = [('H', (0., 0., 0.)), ('H', (0., 0., .7474))]
-    molecule = cudaqlib.operators.create_molecule(
-        geometry, 'sto-3g', 0, 0, verbose=True, casci=True)
+    molecule = cudaqlib.operators.create_molecule(geometry,
+                                                  'sto-3g',
+                                                  0,
+                                                  0,
+                                                  verbose=True,
+                                                  casci=True)
     print(molecule.hamiltonian.to_string())
     print(molecule.energies)
     assert np.isclose(-1.11, molecule.energies['hf_energy'], atol=1e-2)
@@ -26,11 +33,43 @@ def test_operators():
 
 
 
+def test_from_xyz_filename():
+    molecule = cudaqlib.operators.create_molecule(str(currentPath) +
+                                                  '/resources/LiH.xyz',
+                                                  'sto-3g',
+                                                  0,
+                                                  0,
+                                                  verbose=True)
+    print(molecule.energies)
+    print(molecule.n_orbitals)
+    print(molecule.n_electrons)
+    assert molecule.n_orbitals == 6 
+    assert molecule.n_electrons == 4
+
+def test_jordan_wigner():
+
+    geometry = [('H', (0., 0., 0.)), ('H', (0., 0., .7474))]
+    molecule = cudaqlib.operators.create_molecule(geometry,
+                                                  'sto-3g',
+                                                  0,
+                                                  0,
+                                                  verbose=True,
+                                                  casci=True)
+    op = cudaqlib.operators.jordan_wigner(molecule.fermion_operator)
+    print(op.to_string())
+    assert molecule.hamiltonian == op
+    hpq = np.array(molecule.fermion_operator.hpq)
+    hpqrs = np.array(molecule.fermion_operator.hpqrs)
+    hpqJw = cudaqlib.operators.jordan_wigner(hpq, molecule.energies['nuclear_energy'])
+    hpqrsJw = cudaqlib.operators.jordan_wigner(hpqrs)
+    op2 = hpqJw + hpqrsJw 
+    assert op2 == molecule.hamiltonian
+
 
 def test_chemistry_operators():
 
-    @cudaq.kernel 
-    def ansatz(thetas:list[float]):
+    @cudaq.kernel
+    def ansatz(thetas: list[float]):
         q = cudaq.qvector(4)
         x(q[0])
         x(q[1])
@@ -38,11 +77,14 @@ def test_chemistry_operators():
 
     # Define the molecule
     geometry = [('H', (0., 0., 0.)), ('H', (0., 0., .7474))]
-    molecule = cudaqlib.operators.create_molecule(
-        geometry, 'sto-3g', 0, 0, verbose=True)
+    molecule = cudaqlib.operators.create_molecule(geometry,
+                                                  'sto-3g',
+                                                  0,
+                                                  0,
+                                                  verbose=True)
 
     # Run VQE
     energy, params, all_data = cudaqlib.gse.vqe(ansatz,
-                                            molecule.hamiltonian, [0.],
-                                            optimizer='cobyla')
+                                                molecule.hamiltonian, [0.],
+                                                optimizer='cobyla')
     assert np.isclose(-1.137, energy, atol=1e-2)
